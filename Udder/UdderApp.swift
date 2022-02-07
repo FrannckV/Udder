074c062f8584125c14a7cd82d1022f90a754b913
Francisco de Jesus Medrano Villanueva <frann1807@gmail.com>
2022-02-07 14:51:55 -0600

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Frannck Villanueva on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
